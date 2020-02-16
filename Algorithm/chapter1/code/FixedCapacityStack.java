8343fcd0081976b6e3451a625ff97955874e2979
meiyiming <1025926969@qq.com>
2020-02-16 11:46:51 +0800

    Algorithm:chapter1:1.4

---
+++ Algorithm/chapter1/code/FixedCapacityStack.java
@@ -0,0 +1,32 @@
+package Algorithm.chapter1.code;
+
+public class FixedCapacityStack<Item>
+{
+    private Item[] a;
+    private int N;
+
+    public FixedCapacityStack(int cap)
+    {
+        a = (Item[]) new Object[cap];
+    }
+
+    public boolean isEmpty()
+    {
+        return N ==0;
+    }
+
+    public int size()
+    {
+        return N;
+    }
+
+    public void push(Item item)
+    {
+        a[N++] = item;
+    }
+
+    public Item pop()
+    {
+        return a[--N];
+    }
+}
