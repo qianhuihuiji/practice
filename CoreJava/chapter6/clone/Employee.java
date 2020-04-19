fc9148a255dcd1d70b890a0fcfd59cbcf004a099
meiyiming <1025926969@qq.com>
2020-04-19 14:04:08 +0800

    CoreJave:chapter 6:2

--- CoreJava/chapter6/clone/Employee.java
+++ CoreJava/chapter6/clone/Employee.java
@@ -1,4 +1,45 @@
 package CoreJava.chapter6.clone;
 
-public class Employee {
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Employee implements Cloneable{
+    private String name;
+    private double salary;
+    private Date hireDay;
+
+    public Employee(String name, double salary)
+    {
+        this.name = name;
+        this.salary = salary;
+        this.hireDay = new Date();
+    }
+
+    public Employee clone() throws CloneNotSupportedException
+    {
+        Employee cloned = (Employee) super.clone();
+
+        cloned.hireDay = (Date) this.hireDay.clone();
+
+        return cloned;
+    }
+
+    public void setHireDay(int year, int month, int day)
+    {
+        Date newHireDay = new GregorianCalendar(year, month - 1, day).getTime();
+
+        this.hireDay.setTime(newHireDay.getTime());
+    }
+
+    public void raiseSalary(double byPercent)
+    {
+        double raise = salary * byPercent / 100;
+
+        salary += raise;
+    }
+
+    public String toString()
+    {
+        return "Employee[name=" + this.name + ",salary=" + this.salary + ",hireDay=" + this.hireDay + "]";
+    }
 }
