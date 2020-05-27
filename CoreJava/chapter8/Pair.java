e89bb25450fb405bfc7e261fa8a94c48ed780b2a
meiyiming <1025926969@qq.com>
2020-05-27 11:41:40 +0800

    CoreJava:chapter 8

---
+++ CoreJava/chapter8/Pair.java
@@ -0,0 +1,38 @@
+package CoreJava.chapter8;
+
+/**
+ * TODO
+ *
+ * @author nofirst
+ * @date 2020-05-27 10:36
+ */
+public class Pair<T> {
+    private T first;
+    private T second;
+
+    public Pair() {
+        this.first = null;
+        this.second = null;
+    }
+
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setSecond(T second) {
+        this.second = second;
+    }
+}
