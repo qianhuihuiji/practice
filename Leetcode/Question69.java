605f61cdb7be10f2360d0ef771bfbdb9605d02b3
meiyiming <1025926969@qq.com>
2020-08-25 22:29:16 +0800

    LeetCode:Question 69

--- Leetcode/Question69.java
+++ Leetcode/Question69.java
@@ -1,38 +1,29 @@
 package Leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Question69 {
     public static void main(String[] args) {
-        int [] testArr = {2, 7, 11, 15};
-        int target = 9;
-
-
-        int [] answer = twoSum(testArr,target);
-
-
-
-
-        // 打印所有数组元素
-        for (int a : answer) {
-            System.out.println(a);
-        }
+        int answer = mySqrt(2147395599);
+        System.out.println(answer);
     }
 
+    private static int mySqrt(int x) {
+        int maxIntSqrt = 46340;
+        int low = 1;
+        int high = x >= maxIntSqrt ? maxIntSqrt : x;
 
-    private static int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i=0;i<nums.length;i++) {
-            int completement = target - nums[i];
-            if (map.containsKey(completement)) {
-                return new int[]{map.get(completement),i};
+        while (low <= high) {
+            int middle = low + ((high - low) >> 1);
+            int xx = middle * middle;
+            System.out.println(low + "," + high + "," + xx);
+            if (xx < x) {
+                low = middle + 1;
+            } else if (xx > x) {
+                high = middle - 1;
+            } else {
+                return middle;
             }
-            map.put(nums[i], i);
         }
 
-
-        return new int[] {};
+        return low - 1;
     }
-
 }
