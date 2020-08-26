01ef660170f15bf1d62de5b4e930fa15df7ce877
meiyiming <1025926969@qq.com>
2020-08-26 17:32:30 +0800

    LeetCode:Question 350

--- Leetcode/Question350.java
+++ Leetcode/Question350.java
@@ -1,12 +1,13 @@
 package Leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Question350 {
     public static void main(String[] args) {
         int [] arr1 = {1,2,2,1};
-        int [] arr2 = {2};
+        int [] arr2 = {2,2};
 
         int [] answer = intersection(arr1, arr2);
 
@@ -19,50 +20,29 @@ public class Question350 {
 
     private static int[] intersection(int[] nums1, int[] nums2) {
         Map<Integer, Integer> dict = new HashMap<>();
-        for (int a : nums1.length >= nums2.length ? nums2 : nums1) {
-            if (dict.containsKey(a)) {
-                int val = dict.get(a);
-                dict.put(a, val + 1);
+        boolean nums1IsSmaller = nums1.length <= nums2.length;
+
+        // 以小数组构建lookup
+        for (int a : nums1IsSmaller ? nums1 : nums2) {
+            int count = dict.getOrDefault(a, 0) + 1;
+            dict.put(a, count);
+        }
+
+        int[] ans = nums1IsSmaller ? new int[nums1.length] : new int[nums2.length];
+
+        // 遍历大数组
+        int index = 0;
+        for (int a : nums1IsSmaller ? nums2 : nums1) {
+            int count = dict.getOrDefault(a, 0);
+
+            if (count > 0) {
+                dict.put(a, --count);
+                ans[index++] = a;
             } else {
-                dict.put(a, 1);
+                dict.remove(a);
             }
         }
 
-        Map<Integer, Integer> ansMap = new HashMap<>();
-        for (int a : nums1.length >= nums2.length ? nums1 : nums2) {
-            if (dict.containsKey(a)) {
-                int val = dict.get(a);
-
-                if (ansMap.containsKey(a)) {
-                    int ansVal = ansMap.get(a);
-
-                    if (val > 0) {
-                        ansMap.put(a, ansVal + 1);
-                    }
-                } else {
-                    ansMap.put(a, 1);
-                }
-
-                // 这里 ansVal 都会被设置一次值，相应的，val 要扣减一次
-                dict.put(a, val - 1);
-            }
-        }
-
-        int sum = 0;
-        for (Map.Entry<Integer, Integer> key : ansMap.entrySet()) {
-            int value = key.getValue();
-            sum += value;
-        }
-        int[] ans = new int[sum];
-        int i = 0;
-        for (Integer a : ansMap.keySet()) {
-            int val = ansMap.get(a);
-            while (val > 0) {
-                val--;
-                ans[i++] = a;
-            }
-        }
-
-        return ans;
+        return Arrays.copyOfRange(ans, 0, index);
     }
 }
