f5631f131aa579002241c8e2cc25372a4170dbfe
meiyiming <1025926969@qq.com>
2020-09-01 14:51:08 +0800

    LeetCode:Question 2

--- Leetcode/Question2.java
+++ Leetcode/Question2.java
@@ -1,36 +1,81 @@
 package Leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+/**
+ * [2] 两数相加
+ */
 public class Question2 {
     public static void main(String[] args) {
-        int test = 1534236469;
+        ListNode2 node1 = new ListNode2(2);
+        ListNode2 node2 = new ListNode2(4);
+        ListNode2 node3 = new ListNode2(3);
+        node1.next = node2;
+        node2.next = node3;
 
-        System.out.println(reverse(test));
+        ListNode2 node4 = new ListNode2(5);
+        ListNode2 node5 = new ListNode2(6);
+        ListNode2 node6 = new ListNode2(7);
+        node4.next = node5;
+        node5.next = node6;
+
+        ListNode2 ans = addTwoNumbers(node1, node4);
+        while (ans != null) {
+            System.out.println(ans.val);
+            ans = ans.next;
+        }
     }
 
+    private static ListNode2 addTwoNumbers(ListNode2 l1, ListNode2 l2) {
+        if (l1 == null && l2 == null) return null;
 
-    private static int reverse(int x) {
-        int ans = 0;
+        ListNode2 ans = new ListNode2(-1);
+        ListNode2 head = ans;
 
-        while (x != 0) {
-            // 下面这行代码是进行越界判断
-            // 如果 ans * 10 越界，那么 ans * 10 / 10 就会丢失精度，也就不会等于 ans
-            // 那么，会存在 ans * 10 不越界，但是 ans * 10 + x % 10 越界的情况吗？
-            // 答案是不会，因为 ans * 10 不越界，那么 ans * 10 的个位是 0，而 x % 10 的取值是 0 到 9，不会进位
-            if(ans * 10 / 10 != ans) {
-                ans = 0;
+        // 是否需要补位
+        boolean supply = false;
 
-                break;
+        while (l1 != null || l2 != null || supply) {
+            int val = 0;
+            if (l1 != null && l2 != null) {
+                val = l1.val + l2.val;
+                l1 = l1.next;
+                l2 = l2.next;
+            } else if (l1 != null) {
+                val = l1.val;
+                l1 = l1.next;
+            } else if (l2 != null){
+                val = l2.val;
+                l2 = l2.next;
             }
 
-            ans = ans * 10 + x % 10;
-            x /= 10;
+            // 需要补位的话，进行补位，并将补位标志重置
+            if (supply) {
+                val++;
+                supply = false;
+            }
+
+            // 判断本次是否需要补位
+            if (val >= 10) {
+                supply = true;
+                val -= 10;
+            }
+
+            ans.val = val;
+
+            if (l1 != null || l2 != null || supply) {
+                ans.next = new ListNode2(-1);
+                ans = ans.next;
+            }
         }
 
-        return ans;
+        return head;
     }
+
+}
+
+class ListNode2 {
+    int val;
+    ListNode2 next;
+    ListNode2(int val) {
+       this.val = val;
+   }
 }
