79f15963ac587a25b5f2e3bb5733434ab62f95be
meiyiming <1025926969@qq.com>
2020-09-09 14:16:56 +0800

    LeetCode:Question 234

--- Leetcode/Question234_3.java
+++ Leetcode/Question234_3.java
@@ -20,29 +20,71 @@ public class Question234_3 {
     }
 
     private static boolean isPalindrome(ListNode head) {
-        if (head.next == null) return true;
+        if (head == null) return true;
 
         // 使用快慢指针
-        // 找到中间节点，同时反转前半部分链表，然后从中间节点向两边遍历
+        // 找到中间节点，同时反转后半部分链表，然后从头尾两侧向中间遍历
         // 节点个数分奇数和偶数两种情况
-        ListNode slow = head;
-        ListNode fast = head;
-        ListNode middle = slow.next;
-        while (fast.next != null) {
-            // 这种情况说明是偶数个节点，且到了最后一步
-            if (fast.next.next == null) {
-                fast = fast.next;
-            } else {
-                // 反转前半部分链表
-                ListNode tmp = slow.next;
-                slow = slow.next;
-                middle = slow.next;
+        ListNode endOfFirstPart = endOfFirstPart(head);
+        ListNode startOfSecondPartReversed = reverse(endOfFirstPart.next);
 
-                slow.next = tmp;
-                fast = fast.next.next;
-            }
+        ListNode p1 = head;
+        ListNode p2 = startOfSecondPartReversed;
+
+        boolean result = true;
+
+        while (result && p2 != null) {
+            if (p1.val != p2.val) result = false;
+
+            p1 = p1.next;
+            p2 = p2.next;
         }
 
-        return true;
+        // 恢复原来链表的顺序
+        endOfFirstPart.next = reverse(startOfSecondPartReversed);
+
+        return result;
+    }
+
+    /**
+     * 反转链表
+     *
+     * @param node
+     * @return
+     */
+    private static ListNode reverse(ListNode node) {
+        ListNode pre = null;
+        ListNode cur = node;
+
+        while (cur != null) {
+            ListNode tmp = cur.next;
+
+            // 反转
+            cur.next = pre;
+
+            // 前进
+            pre = cur;
+            cur = tmp;
+        }
+
+        return pre;
+    }
+
+    /**
+     * 使用快慢指针找到中间节点，同是也是第一个区域的最后一个节点
+     *
+     * @param head
+     * @return
+     */
+    private static ListNode endOfFirstPart(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null  && fast.next.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+        }
+
+        return slow;
     }
 }
