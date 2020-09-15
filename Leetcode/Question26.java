bf623bb4c212300c8f6c27cdedd030e65f4896a8
meiyiming <1025926969@qq.com>
2020-09-15 12:54:26 +0800

    LeetCode:Question 26

--- Leetcode/Question26.java
+++ Leetcode/Question26.java
@@ -2,7 +2,7 @@ package Leetcode;
 
 public class Question26 {
     public static void main(String[] args) {
-        int[] test = new int[] {0,0,1,1,1,2,2,3,3,4};
+        int[] test = new int[] {0,0,0,1,1,1,2,2,3,3,4};
         int ans = removeDuplicates(test);
         System.out.println();
         System.out.println(ans);
@@ -12,26 +12,17 @@ public class Question26 {
     }
 
     private static int removeDuplicates(int[] nums) {
-        if (nums.length == 0) return 0;
+        if (nums.length <= 1) return nums.length;
 
         int i = 0;
         for (int j = 1;j < nums.length;j++) {
             if (nums[i] != nums[j]) {
+                // 一旦两者不相等，说明 i 的右边需要换成新的值，因此 i++
                 i++;
                 nums[i] = nums[j];
-                System.out.print("当前：j=" + j + " ");
-                printArr(nums);
             }
         }
 
         return i + 1;
-    }
-
-    private static void printArr(int[] arr) {
-        for (int a: arr
-             ) {
-            System.out.print(a + " ");
-        }
-        System.out.println();
     }
 }
