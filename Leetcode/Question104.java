3586f88aeca57ef6f62f7fe3ae34396bd342662e
meiyiming <1025926969@qq.com>
2020-09-20 14:51:36 +0800

    LeetCode:Question 104

---
+++ Leetcode/Question104.java
@@ -0,0 +1,28 @@
+package Leetcode;
+
+/**
+ * [104] 二叉树的最大深度
+ */
+public class Question104 {
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(20);
+        TreeNode node4 = new TreeNode(15);
+        TreeNode node5 = new TreeNode(7);
+        node1.left = node2;
+        node1.right = node3;
+        node3.left = node4;
+        node3.right = node5;
+
+        int ans = maxDepth(node1);
+
+        System.out.println(ans);
+    }
+
+    private static int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
---
+++ Leetcode/TreeNode.java
@@ -0,0 +1,14 @@
+package Leetcode;
+
+/**
+ * Definition for a binary tree node.
+ *
+ * @author nofirst
+ * @date 2020-09-20 14:53
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
