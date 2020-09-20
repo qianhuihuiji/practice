ebca1429f3d99271920f4e86dc930b92b25b10d0
meiyiming <1025926969@qq.com>
2020-09-20 15:18:02 +0800

    LeetCode:Question 110

---
+++ Leetcode/Question110_2.java
@@ -0,0 +1,45 @@
+package Leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * [110] 平衡二叉树
+ */
+public class Question110_2 {
+    private static Map<TreeNode, Integer> lookup = new HashMap<>();
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(20);
+        TreeNode node4 = new TreeNode(15);
+        TreeNode node5 = new TreeNode(7);
+        node1.left = node2;
+        node1.right = node3;
+        node3.left = node4;
+        node3.right = node5;
+
+        boolean ans = isBalanced(node1);
+
+        System.out.println(ans);
+    }
+
+    private static boolean isBalanced(TreeNode root) {
+
+        if (root == null) return true;
+
+        return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private static int height(TreeNode root) {
+        if (root == null) return 0;
+
+        if (lookup.containsKey(root)) return lookup.get(root);
+
+        int height = Math.max(height(root.left), height(root.right)) + 1;
+
+        lookup.put(root, height);
+
+        return height;
+    }
+}
---
+++ Leetcode/Question110_3.java
@@ -0,0 +1,39 @@
+package Leetcode;
+
+/**
+ * [110] 平衡二叉树
+ */
+public class Question110_3 {
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(20);
+        TreeNode node4 = new TreeNode(15);
+        TreeNode node5 = new TreeNode(7);
+        node1.left = node2;
+        node1.right = node3;
+        node3.left = node4;
+        node3.right = node5;
+
+        boolean ans = isBalanced(node1);
+
+        System.out.println(ans);
+    }
+
+    private static boolean isBalanced(TreeNode root) {
+        return height(root) >= 0;
+    }
+
+    private static int height(TreeNode root) {
+        if (root == null) return 0;
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(height(root.left), height(root.right)) + 1;
+    }
+}
