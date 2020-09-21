6ceca803eb8c12f34336ca4beeb730ac603b7473
meiyiming <1025926969@qq.com>
2020-09-21 15:52:17 +0800

    LeetCode:Question 107

---
+++ Leetcode/Question107.java
@@ -0,0 +1,59 @@
+package Leetcode;
+
+import java.util.*;
+
+/**
+ * [107] 二叉树的层次遍历 II
+ */
+public class Question107 {
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(20);
+        TreeNode node4 = new TreeNode(15);
+        TreeNode node5 = new TreeNode(7);
+        node1.left = node2;
+        node1.right = node3;
+        node3.left = node4;
+        node3.right = node5;
+
+        List<List<Integer>> ans = levelOrderBottom(node1);
+
+        ans.forEach(list -> list.forEach(System.out::println));
+    }
+
+    private static List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if (root == null) {
+            return ret;
+        }
+
+        Map<Integer, List<Integer>> lookup = new HashMap<>();
+
+        int level = 1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int currentLevelSize = queue.size();
+            for (int i = 1; i <= currentLevelSize; ++i) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            lookup.put(level, list);
+            level++;
+        }
+
+        for (int j = level - 1;j > 0;j--) {
+            ret.add(lookup.get(j));
+        }
+
+        return ret;
+    }
+}
