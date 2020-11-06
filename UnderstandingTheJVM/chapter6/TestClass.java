abb3d7c3ec2a66912c562f571d7e1a1c437caa3b
meiyiming <1025926969@qq.com>
2020-11-06 16:49:50 +0800

    UnderstandingJVM:chapter 6

---
+++ UnderstandingTheJVM/chapter6/TestClass.java
@@ -0,0 +1,14 @@
+package UnderstandingTheJVM.chapter6;
+
+/**
+ * 第 6 章类文件结构的示例类
+ *
+ * @author nofirst
+ * @date 2020-11-05 16:03
+ */
+public class TestClass {
+    private int m;
+    public int inc () {
+        return m + 1;
+    }
+}
