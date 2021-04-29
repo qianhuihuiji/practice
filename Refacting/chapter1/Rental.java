97d9310f0cc80016f3f311bf1dcd180bafcad62b
meiyiming <1025926969@qq.com>
2021-04-29 23:43:03 +0800

    Refactoring:page 35

--- Refacting/chapter1/Rental.java
+++ Refacting/chapter1/Rental.java
@@ -18,26 +18,7 @@ public class Rental {
     }
 
     protected double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2) {
-                    result += (getDaysRented() - 2) * 2.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (getDaysRented() > 3) {
-                    result += (getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-
-        return result;
+        return _movie.getCharge(_daysRented);
     }
 
     protected int getFrequentRenterPoints() {
