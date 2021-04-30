f7b023e3e5974cece30a69b5861d3493b8764a95
qianhuihuiji <1025926969@qq.com>
2021-04-30 10:56:24 +0800

    Refacting:page 46

--- Refacting/chapter1/Movie.java
+++ Refacting/chapter1/Movie.java
@@ -7,18 +7,31 @@ public class Movie {
 
     private String _title;
     private int _priceCode;
+    private Price _price;
 
-    public Movie(String _title, int _priceCode) {
+    public Movie(String _title, int _priceCode) throws IllegalAccessException {
         this._title = _title;
-        this._priceCode = _priceCode;
+        setPriceCode(_priceCode);
     }
 
     public int getPriceCode() {
         return _priceCode;
     }
 
-    public void setPriceCode(int priceCode) {
-        this._priceCode = priceCode;
+    public void setPriceCode(int arg) throws IllegalAccessException {
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalAccessException("Incorrect Price Code");
+        }
     }
 
     public String getTitle() {
@@ -26,26 +39,7 @@ public class Movie {
     }
 
     protected double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2) {
-                    result += (daysRented - 2) * 2.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3) {
-                    result += (daysRented - 3) * 1.5;
-                }
-                break;
-        }
-
-        return result;
+        return _price.getCharge(daysRented);
     }
 
     protected int getFrequentRenterPoints(int daysRented) {
